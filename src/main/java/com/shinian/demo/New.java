10e84727d02e11d3b9995464514c933c56931f03
shinian <774328384@qq.com>
2017-10-17 10:45:07 +0800

    增加ali约束

--- src/main/java/com/shinian/demo/New.java
+++ src/main/java/com/shinian/demo/New.java
@@ -1,6 +1,13 @@
 package com.shinian.demo;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 
 /**
  * @author shinian
@@ -11,23 +18,25 @@ import java.util.*;
  * @date 2017/8/16 10:03
  */
 public class New {
+    private static final int SIZE = 5;
+
     public static <K, V> Map<K, V> map() {
-        return new HashMap<K,V>();
+        return new HashMap<K, V>(SIZE);
     }
 
-    public static <T> List<T> list(){
-        return new ArrayList<T>();
+    public static <T> List<T> list() {
+        return new ArrayList<T>(SIZE);
     }
 
-    public static <T> List<T> lList(){
+    public static <T> List<T> lList() {
         return new LinkedList<T>();
     }
 
-    public static <T> Set<T> set(){
-        return new HashSet<T>();
+    public static <T> Set<T> set() {
+        return new HashSet<T>(SIZE);
     }
 
-    public static <T> Queue<T> queue(){
+    public static <T> Queue<T> queue() {
         return new LinkedList<T>();
     }
 
