1db38864b0333ffcc7c786941391bc70b8a20822
shinian <774328384@qq.com>
2017-10-20 18:56:16 +0800

    400

---
+++ src/main/java/com/shinian/demo/MultioleInterfaceVariants.java
@@ -0,0 +1,22 @@
+package com.shinian.demo;
+
+/**
+ * @author shinian
+ * @date 2017/10/20/020
+ * do not compile
+ */
+//public class MultioleInterfaceVariants {
+//
+//
+//}
+//
+//interface Payable<T> {
+//}
+//
+//class Employee1 implements Payable<Employee1> {
+//}
+//
+//class Hourly extends Employee1 implements Payable<Hourly> {
+//}
+
+
